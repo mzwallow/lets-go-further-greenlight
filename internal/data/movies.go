a788d9119353b03bec20e9e5ac6ced752d386132
mzwallow <punnarat_mos@hotmail.com>
2025-01-10 15:53:36 +0700

    feat: 3.3 encoding structs

---
+++ internal/data/movies.go
@@ -0,0 +1,16 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
+
+// Note that the string directive will only work on struct fields which have int*, uint*, float* or bool types.
+// `json:"rumntime,omitempty,string"`
