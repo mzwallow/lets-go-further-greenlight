80c28d1a2cca236c87213fe85d037483fee37fcf
mzwallow <punnarat_mos@hotmail.com>
2025-02-07 10:58:12 +0700

    feat: ch8.2 optimistic concurrency control

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -45,6 +45,11 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
+
 func (app *application) failedValidationResponse(
 	w http.ResponseWriter,
 	r *http.Request,
